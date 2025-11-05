512e6bd1d581d642a0e047b0936f90f103b76c79
FRANKYMIGUEL <iscfrankgonzalez@gmail.com>
2025-11-05 17:58:12 -0600

    Trabajo en clase con funciones de ordenamiento

--- 1025/seleccionshort.py
+++ 1025/seleccionshort.py
@@ -2,17 +2,16 @@
 # vi: set shiftwidth=4 tabstop=8 expandtab:
 """
 Algoritmo de Ordenamiento por Selección (Selection Sort)
-Trabajo en clase
 """
 import random
 import time
 
-cantidad_de_elmentos = 100
+cantidad_de_elementos = 100
 inicio = 0
 fin = 100
 
 
-def ordenar_seleccion(arr):
+def ordenar(arr):
     # Obtener la longitud de la lista
     longitud = len(arr)
     # Iterar sobre cada elemento de la lista
@@ -33,15 +32,19 @@ def main():
     """
     Función principal
     """
-    lista = [random.randint(inicio,fin) for _ in range(cantidad_de_elmentos) ]
+    lista = [random.randint(inicio,fin) for _ in range(cantidad_de_elementos) ]
     ordenada = lista.copy()
 
-    tiempo_inicio = time.time()
-    ordenar_seleccion(ordenada)
-    tiempo_fin = time.time()
+    #tiempo_inicio = time.time()
+    #ordenar_seleccion(ordenada)
+    #tiempo_fin = time.time()
+    tiempo_inicio_ns = time.perf_counter_ns()
+    ordenar(ordenada)
+    tiempo_fin_ns = time.perf_counter_ns()
 
-    duracion = tiempo_fin - tiempo_inicio
-    print(f"La función se ejecutó en: {duracion:.4f} segundos.")
+    duracion_us = (tiempo_fin_ns - tiempo_inicio_ns) / 1000  # Convirtiendo de ns a us
+    #print(f"La función se ejecutó en: {duracion:.6f} segundos.")
+    print(f"La función se ejecutó en: {duracion_us:.3f} microsegundos.")
     print("Lista desordenada", lista)
     print("Lista ordenada", ordenada)
 if __name__ == "__main__":
--- 1025/todos.py
+++ 1025/todos.py
@@ -9,6 +9,7 @@ import os
 
 from quicksort import ordenar as qs
 from merge_sort import ordenar as ms
+from seleccionshort import ordenar as ss
 
 # 100, 1_000, 10_000, 100_000, 1_000_000
 cantidad_de_elementos = 1_000_000
@@ -18,7 +19,7 @@ fin = cantidad_de_elementos
 nombre_de_archivo_de_tiempos = "tiempos.csv"
 
 def verifica(lista, algoritmo="sin algoritmo"):
-    mayor = -10;
+    mayor = -10
     for elemento in lista:
         if elemento >= mayor:
             mayor = elemento
@@ -42,21 +43,31 @@ def main():
     Función principal
     """
     lista = [random.randint(inicio,fin) for _ in range(cantidad_de_elementos) ]
-
-    duracion_qs = obtener_tiempo("Quicksort", qs, lista)
-    duracion_ms = obtener_tiempo("Merge sort", ms, lista)
-
+    print(f"Ordenando {cantidad_de_elementos:_} elementos")
+    #duracion_qs = obtener_tiempo("Quicksort", qs, lista)
+    print("-----")
+    #duracion_ms = obtener_tiempo("Merge sort", ms, lista)
+    print("-----")
+    duracion_ss = obtener_tiempo("Selection sort", ss, lista)
+    print("-----")
+    #print(f"Quicksort duró {duracion_qs:_} microsegundos")
+    #print(f"Merge sort duró {duracion_ms:_} microsegundos")
+    print(f"Selection sort duró {duracion_ss:_} microsegundos")
+   
     es_nuevo = True
     if os.path.exists(nombre_de_archivo_de_tiempos):
-        es_nuevo = False
-
+            es_nuevo = False
     with open(nombre_de_archivo_de_tiempos,"a", encoding="utf-8") as archivo:
         if es_nuevo:
             archivo.write("Algoritmo,Cantidad de elementos,Tiempo\n")
-        archivo.write(f"Quicksort,{cantidad_de_elementos:_},{duracion_qs:_}\n")
-        archivo.write(f"Merge sort,{cantidad_de_elementos:_},{duracion_ms:_}\n")
-
-
+            #archivo.write(f"Quicksort,{cantidad_de_elementos:_},{duracion_qs:_}\n")
+            #archivo.write(f"Merge sort,{cantidad_de_elementos:_},{duracion_ms:_}\n")
+            archivo.write(f"Selection sort,{cantidad_de_elementos:_},{duracion_ss:_}\n")
+        else:
+            #archivo.write("Algoritmo,Cantidad de elementos,Tiempo\n")
+            #archivo.write(f"Quicksort,{cantidad_de_elementos:_},{duracion_qs:_}\n")
+            #archivo.write(f"Merge sort,{cantidad_de_elementos:_},{duracion_ms:_}\n")
+            archivo.write(f"Selection sort,{cantidad_de_elementos:_},{duracion_ss:_}\n")
 
 if __name__ == "__main__":
     main()
