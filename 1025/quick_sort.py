02fe8107adceb903c0879fe764f072dd2b39e143
LonelyLie7 <leynad367_sg@outlook.es>
2025-11-08 12:13:49 -0600

    Agregando algoritmos faltantes
    Se agregaron bubble sort, insertion sort y heapsort al archivo "ordering_methods_luis_daniel_sainz"
    Se agregó un condicional para no ejecutar algoritmos con complejidad O(n^2) para ordenar 1_000_000 de elementos
    Se renombró el archivo "tarea_luis_daniel.csv" a "tiempos_luis_daniel_sainz.csv"
    Se hizo un cambio en el archivo "quick_sort.py" para mejorar la forma en que se elije el pivote

--- 1025/quick_sort.py
+++ 1025/quick_sort.py
@@ -1,6 +1,14 @@
+def choose_pivot(arr):
+    first = arr[0]
+    middle = arr[len(arr)//2]
+    last = arr[-1]
+    # Devolver la mediana de los tres
+    return sorted([first, middle, last])[1]
+
 def sort(arr):
     if len(arr) <= 1:
         return arr
-    pivot = arr[len(arr)//2]
+    pivot = choose_pivot(arr)
+
     return sort([x for x in arr[1:] if x < pivot]) + [pivot] \
         + sort([x for x in arr[1:] if x >= pivot])
