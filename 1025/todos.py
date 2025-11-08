00488fdd1c336c56da70e42253187adbbebd777d
Giova Torres <giova2989@hotmail.com>
2025-11-08 09:21:41 -0600

    Subo el archivo de todos

--- 1025/todos.py
+++ 1025/todos.py
@@ -9,6 +9,7 @@ import os
 
 from quicksort import ordenar as qs
 from merge_sort import ordenar as ms
+from countingsort import ordenar as cs
 
 # 100, 1_000, 10_000, 100_000, 1_000_000
 cantidad_de_elementos = 1_000_000
@@ -18,7 +19,7 @@ fin = cantidad_de_elementos
 nombre_de_archivo_de_tiempos = "tiempos.csv"
 
 def verifica(lista, algoritmo="sin algoritmo"):
-    mayor = -10;
+    mayor = -10
     for elemento in lista:
         if elemento >= mayor:
             mayor = elemento
@@ -42,21 +43,26 @@ def main():
     Función principal
     """
     lista = [random.randint(inicio,fin) for _ in range(cantidad_de_elementos) ]
-
+    print(f"Ordenando {cantidad_de_elementos:_} elementos")
     duracion_qs = obtener_tiempo("Quicksort", qs, lista)
     duracion_ms = obtener_tiempo("Merge sort", ms, lista)
-
+    duracion_cs = obtener_tiempo("Counting sort", cs, lista)
+    
+   
     es_nuevo = True
     if os.path.exists(nombre_de_archivo_de_tiempos):
-        es_nuevo = False
-
+            es_nuevo = False
     with open(nombre_de_archivo_de_tiempos,"a", encoding="utf-8") as archivo:
         if es_nuevo:
             archivo.write("Algoritmo,Cantidad de elementos,Tiempo\n")
-        archivo.write(f"Quicksort,{cantidad_de_elementos:_},{duracion_qs:_}\n")
-        archivo.write(f"Merge sort,{cantidad_de_elementos:_},{duracion_ms:_}\n")
-
-
+            archivo.write(f"Quicksort,{cantidad_de_elementos:_},{duracion_qs:_}\n")
+            archivo.write(f"Merge sort,{cantidad_de_elementos:_},{duracion_ms:_}\n")
+            archivo.write(f"Counting sort,{cantidad_de_elementos:_},{duracion_cs:_}\n")
+        else:
+            archivo.write("Algoritmo,Cantidad de elementos,Tiempo\n")
+            archivo.write(f"Quicksort,{cantidad_de_elementos:_},{duracion_qs:_}\n")
+            archivo.write(f"Merge sort,{cantidad_de_elementos:_},{duracion_ms:_}\n")
+            archivo.write(f"Counting sort,{cantidad_de_elementos:_},{duracion_cs:_}\n")
 
 if __name__ == "__main__":
     main()
